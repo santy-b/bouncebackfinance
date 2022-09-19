403018390835350c5657329ac7943435f3ef149f
Brian Figueroa <sbrianfig@gmail.com>
2022-09-19 15:19:27 -0400

    Added new static files folder.

--- bouncebackfinance/settings.py
+++ bouncebackfinance/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*-w_v9zyk1g=7-#bach^uar*q^6yzpug89*v=0rp0ta3zk3v)7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bouncebackfinance.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
